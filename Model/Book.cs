9caec0fd02f684354d435840af3ce9515ccce98a
Jubayed Ahmed <jubayedsr@gmail.com>
2020-03-27 00:34:20 +0600

    Update
    Some Design Changes
    Added BookList tab

---
+++ Model/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace BookListRazorWebApp.Model
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Author { get; set; }
+    }
+}
